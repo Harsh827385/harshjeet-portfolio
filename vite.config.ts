79c8fdff37bd4c4c383dbafac8fc8f0181b6280a
Harsh827385 <harshjeetthakur66@gmail.com>
2026-07-02 23:57:51 +0530

    Configure GitHub Pages deployment and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
-  base: '/',
+  base:'/harshjeet-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
